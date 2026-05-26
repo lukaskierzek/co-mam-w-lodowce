68a676ef9e4fe606b8fd8a857d31640cb08517f3
lukaskierzek <lukas.kierzek@gmail.com>
2026-05-26 20:04:24 +0200

    add hotjar

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,48 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+
+import Hotjar from '@hotjar/browser'
+
 import './index.css'
+
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 
-createRoot(document.getElementById('root')).render(
+const siteId =
+  import.meta.env
+    .VITE_HOTJAR_ID
+
+const hotjarVersion = 6
+
+if (
+  import.meta.env.PROD &&
+  siteId
+) {
+
+  Hotjar.init(
+    siteId,
+    hotjarVersion
+  )
+
+  console.log(
+    'Hotjar uruchomiony'
+  )
+
+}
+
+createRoot(
+  document.getElementById(
+    'root'
+  )
+).render(
+
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
+
 )
